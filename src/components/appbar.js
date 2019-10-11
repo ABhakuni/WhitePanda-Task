04053c3092d75ec9b7b4b47940c5f0f4eeddf8d4
A_Bhakuni <anurag.bhakuni@gmail.com>
2019-10-12 05:08:40 +0530

    whitepanda

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import Badge from '@material-ui/core/Badge';
 import Avatar from '@material-ui/core/Avatar';
 import { NotificationsActiveOutlined } from '@material-ui/icons';
